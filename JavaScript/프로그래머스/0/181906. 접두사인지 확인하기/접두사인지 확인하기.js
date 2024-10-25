778f501b54137172fc356ba928d53ab143120584
Bubbley-dev <sehee4329@gmail.com>
2024-10-25 13:46:43 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -0,0 +1,8 @@
+function solution(my_string, is_prefix) {
+    const prefix = []
+    for (let i=1; i<=my_string.length; i++) {
+        prefix.push(my_string.slice(0, i))
+    }
+    
+    return prefix.includes(is_prefix) ? 1 : 0;
+}
