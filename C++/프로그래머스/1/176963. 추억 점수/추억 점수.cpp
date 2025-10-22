c97cfe81de2bd373f3c0aa3af5b059a6351c24ff
Bubbley <sehee4329@gmail.com>
2025-10-22 20:15:04 +0900

    [level 1] Title: 추억 점수, Time: 1.86 ms, Memory: 4.26 MB -BaekjoonHub

---
+++ C++/프로그래머스/1/176963. 추억 점수/추억 점수.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
+    vector<int> answer;
+    map<string, int> m;
+    for(int i=0; i<name.size(); i++)
+    {
+        m[name[i]] = yearning[i];
+    }
+    
+    for (auto p : photo)
+    {
+        int score = 0;
+        for (string n : p)
+        {
+            if (m.find(n) != m.end()) score += m[n];
+        }
+        answer.push_back(score);
+    }
+    
+    return answer;
+}
