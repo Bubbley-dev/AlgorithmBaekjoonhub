b3d750afba8e0a7f97869466cc2875437e690671
Bubbley <sehee4329@gmail.com>
2025-11-18 19:31:43 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.53 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ C#/프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int[] solution(long n) {
+        string s = n.ToString();
+        int[] answer = new int[s.Length];
+        
+        int i = 0;
+        while (n>0) {
+            answer[i++] = (int)(n % (long)10);
+            n /= 10;
+        }
+        
+        return answer;
+    }
+}
