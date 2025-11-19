357be8494dd5ea1ec1701532218975273aa86904
Bubbley <sehee4329@gmail.com>
2025-11-19 14:48:53 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.64 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int k) {
+        string answer = "";
+        
+        while (k-- > 0)
+        {
+            answer += my_string;
+        }
+        
+        return answer;
+    }
+}
